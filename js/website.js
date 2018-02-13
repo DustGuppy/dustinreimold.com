922ef0d489907aa9abd89ea008a02596b1a66874
Dustin Reimold <dustinreimold@gmail.com>
2018-02-13 00:04:38 -0500

    fixed about and project modals so they both open and close

--- js/website.js
+++ js/website.js
@@ -1,26 +1,32 @@
-var modal;
-var btn;
-var span;
+document.getElementById("about").onclick =  function(){openAboutModal()};
+document.getElementById("projects").onclick = function(){openProjectModal()};
+document.getElementById("closeAbout").onclick = function(){closeAboutModal()};
+document.getElementById("closeProject").onclick = function(){closeProjectModal()};
 
-if(document.getElementById == "aboutModal"){
-  modal = document.getElementById("aboutModal");
-  btn = document.getElementById("about");
-  span = document.getElementsByClassName("close");
-}
-else if (document.getElementById == "projectModal") {
-  modal = document.getElementById("projectModal");
-  btn = document.getElementById("projects");
-  span = document.getElementsByClassName("close2");
-}
+function openAboutModal(){
+    document.getElementById('aboutModal').style.display = "block";
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) { // FIXME: close button does not close modal
-    if (event.target == modal) {
-        modal.style.display = "none";
+    window.onclick = function(event){
+      if(event.target == document.getElementById('aboutModal')){
+      document.getElementById('aboutModal').style.display = "none";
     }
+  }
+}
+
+function closeAboutModal(){
+  document.getElementById('aboutModal').style.display ="none";
+}
+
+function openProjectModal(){
+    document.getElementById('projectModal').style.display = "block";
+
+    window.onclick = function(event){
+      if(event.target == document.getElementById('projectModal')){
+      document.getElementById('projectModal').style.display = "none";
+    }
+  }
+}
+
+function closeProjectModal(){
+  document.getElementById('projectModal').style.display ="none";
 }
